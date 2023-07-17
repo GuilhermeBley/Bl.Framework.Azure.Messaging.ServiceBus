1001b57f0d62e51e7e5c3f257869003e5e7f0162
Guilherme Bley <guilhermecbley@gmail.com>
2023-07-17 13:35:36 -0300

    add publisher

---
+++ Bl.Framework.Azure.Messaging.ServiceBus/Bus/Publisher.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Security.Policy;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bl.Framework.Azure.Messaging.ServiceBus.Bus
+{
+    internal class Publisher : IAsyncDisposable, IDisposable
+    {
+        private readonly ServiceBusClient _client;
+        private readonly ServiceBusSender _sender;
+
+        public Publisher(string connectionString, string queueName)
+        {
+            _client = new ServiceBusClient(connectionString);
+            _sender = _client.CreateSender(queueName);
+        }
+
+        public void Dispose()
+            => DisposeAsync().GetAwaiter().GetResult();
+        
+        public async ValueTask DisposeAsync()
+        {
+            await _sender.DisposeAsync();
+            await _client.DisposeAsync();
+        }
+
+        public async Task Publish(string message, CancellationToken cancellationToken = default)
+        {
+            await SendMessage(message, cancellationToken).ConfigureAwait(false);
+        }
+
+        private async Task SendMessage(string bodyMessage, CancellationToken cancellationToken)
+        {
+            var message = new ServiceBusMessage(bodyMessage)
+            {
+                
+            };
+
+            await _sender.SendMessageAsync(message, cancellationToken);
+        }
+    }
+}
--- Bl.Framework.Azure.Messaging.ServiceBus/Program.cs
+++ Bl.Framework.Azure.Messaging.ServiceBus/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Bl.Framework.Azure.Messaging.ServiceBus
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Reflection;
+
+namespace Bl.Framework.Azure.Messaging.ServiceBus
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddUserSecrets(Assembly.GetExecutingAssembly())
+                .Build();
+            
+            var azureServiceBusConnectionString = configurationRoot.GetConnectionString("AzServiceBus");
+            
+
+
+            
         }
     }
 }
