fd748d031b7beef4b556c957fd6d80abece60958
Guilherme Bley <guilhermecbley@gmail.com>
2023-07-17 14:47:02 -0300

    pub and cons

---
+++ Bl.Framework.Azure.Messaging.ServiceBus/Bus/Consumer.cs
@@ -0,0 +1,60 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bl.Framework.Azure.Messaging.ServiceBus.Bus
+{
+    internal class Consumer : IDisposable, IAsyncDisposable
+    {
+
+        private readonly ServiceBusClient _client;
+        private readonly string _queueName;
+
+        public Consumer(string connectionString, string queueName)
+        {
+            _client = new ServiceBusClient(connectionString);
+            _queueName = queueName;
+        }
+
+        public void Dispose()
+            => DisposeAsync().GetAwaiter().GetResult();
+
+        public async ValueTask DisposeAsync()
+        {
+            await _client.DisposeAsync();
+        }
+
+        public void CreateThreadConsumer(Action<ServiceBusReceivedMessage> action, CancellationToken cancellationToken = default)
+        {
+            var thread = new Thread(new ThreadStart(()=>ConsumeTaskMethod(action, cancellationToken)));
+
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void ConsumeTaskMethod(Action<ServiceBusReceivedMessage> act, CancellationToken cancellationToken)
+        {
+            ServiceBusReceiver receiver = null;
+
+            try
+            {
+                receiver = _client.CreateReceiver(_queueName);
+
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    var message = receiver.ReceiveMessageAsync().GetAwaiter().GetResult();
+
+                    act(message);
+
+                    Thread.Sleep(50);
+                }
+            }
+            finally
+            {
+                if (receiver != null)
+                    receiver.DisposeAsync().GetAwaiter().GetResult();
+            }
+        }
+    }
+}
--- Bl.Framework.Azure.Messaging.ServiceBus/Program.cs
+++ Bl.Framework.Azure.Messaging.ServiceBus/Program.cs
@@ -1,23 +1,61 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Bl.Framework.Azure.Messaging.ServiceBus.Bus;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Concurrent;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Bl.Framework.Azure.Messaging.ServiceBus
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var configurationRoot = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddUserSecrets(Assembly.GetExecutingAssembly())
                 .Build();
-            
+
             var azureServiceBusConnectionString = configurationRoot.GetConnectionString("AzServiceBus");
-            
 
+            Console.Write("Type message: ");
+            var messageLine = Console.ReadLine();
 
-            
+            Console.Write("Type message quantity (number 1 - 100): ");
+            var readCountText = Console.ReadLine();
+
+            int.TryParse(readCountText, out int readCount);
+
+            readCount = readCount < 1 || readCount > 100 ? 10 : readCount;
+
+            var messagesConsole = new ConcurrentQueue<string>();
+
+            using (var consumer = new Consumer(azureServiceBusConnectionString, "queue-example"))
+            {
+                using (var pub = new Publisher(azureServiceBusConnectionString, "queue-example"))
+                {
+                    for (int messageQtt = 0; messageQtt < readCount; messageQtt++)
+                        await pub.Publish(messageLine);
+                }
+
+                consumer.CreateThreadConsumer(
+                    (m) =>
+                    {
+                        messagesConsole.Enqueue(Encoding.UTF8.GetString(m.Body.ToArray()));
+                    });
+                
+                while (true)
+                {
+                    await Task.Delay(TimeSpan.FromMilliseconds(100));
+                    var messageToWrite = messagesConsole.FirstOrDefault();
+
+                    if (messageToWrite != null)
+                        Console.WriteLine(messageToWrite);
+                }
+            }
         }
     }
 }
